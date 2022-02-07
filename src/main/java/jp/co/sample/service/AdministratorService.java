2ba22e9d9be925ea2ead619bf3e0ec32dfa2d5bf
kashimamiyu <pg.studykm@gmail.com>
2022-02-07 15:36:16 +0900

    modified AdministratorService

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,8 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
+/**
+ * 管理者情報を登録する業務処理を行うメソッドを作成します。
+ * 
+ * @author kashimamiyu
+ *
+ */
 @Service
 @Transactional
 public class AdministratorService {
@@ -13,5 +20,14 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
 	
+	/**
+	 * 管理者情報を挿入する。
+	 * @param administrator
+	 */
+	public void insert(Administrator administrator) {
+		
+		administratorRepository.insert(administrator);
+		
+	}
 	
 }
