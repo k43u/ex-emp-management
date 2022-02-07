b2923607b1f2ecf59bec2181cb8e5af18ae70f31
kashimamiyu <pg.studykm@gmail.com>
2022-02-07 17:50:02 +0900

    modified AdministratorController and login.html

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 /**
@@ -53,5 +54,16 @@ public class AdministratorController {
 		administrator.setPassword(form.getPassword());
 		administratorService.insert(administrator);
 		return "/";
+	}
+	
+	@ModelAttribute
+	LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+	
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login";
+		
 	}
 }
