7c40d93b9a2fac0c6f3b804e2e8c147c748cd64d
kashimamiyu <pg.studykm@gmail.com>
2022-02-07 15:01:29 +0900

    create AdministratorController

---
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -0,0 +1,41 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+/**
+ * 管理者登録画面を表示する処理を行います。
+ * 
+ * @author kashimamiyu
+ *
+ */
+
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+
+	@Autowired
+	private AdministratorService administratorService;
+	
+	/**
+	 * @return 
+	 */
+	
+	@ModelAttribute
+	public InsertAdministratorForm setUpAdministratorForm(){
+		return new InsertAdministratorForm();
+	}
+	
+	/**
+	 * @return administrator/insert.htmlにフォワード
+	 */
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert";
+	}
+}
