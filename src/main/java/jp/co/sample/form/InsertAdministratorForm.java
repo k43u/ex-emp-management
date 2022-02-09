350c300a2921d7b859d6de1d3b94007169495def
kashimamiyu <pg.studykm@gmail.com>
2022-02-09 16:55:28 +0900

    modified pom.xml, AdministratorController, InsertAdministratorForm, insert.html

--- src/main/java/jp/co/sample/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package jp.co.sample.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム
  * 
@@ -9,10 +13,16 @@ package jp.co.sample.form;
 public class InsertAdministratorForm {
 	
 	/**名前*/
+	@NotBlank(message="名前は必須です")
 	private String name;
 	/**メールアドレス*/
+	@NotBlank(message="メールアドレスは必須です")
+	@Size(min=1, max=127, message="Eメールは1文字以上127文字以内で記載してください")
+	@Email(message="Eメールの形式が不正です")
 	private String mailAddress;
 	/**パスワード*/
+	@NotBlank(message="パスワードは必須です")
+	@Size(min=1, max=127, message="パスワードは1文字以上127文字以内で記載してください")
 	private String password;
 	
 	public String getName() {
